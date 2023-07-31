082f832f7c3bd985aa3b1a5c9957d69a1867af20
ZQDesigned <2990918167@QQ.COM>
2023-07-31 14:38:42 +0800

    SQL::Update：更新数据库内容和映射实体类

--- go/entity/SQLUser.go
+++ go/entity/SQLUser.go
@@ -2,12 +2,14 @@ package entity
 
 // SQLUser 数据库中的用户表
 type SQLUser struct {
-	UserId     int    `gorm:"column:userid;primary_key"`
-	UserName   string `gorm:"column:username"`
-	Password   string `gorm:"column:passwd"`
-	Avatar     string `gorm:"column:avatar"`
-	Background string `gorm:"column:background"`
-	Signature  string `gorm:"column:signature"`
+	UserId        int64  `gorm:"column:userid;primary_key"`
+	UserName      string `gorm:"column:username"`
+	Password      string `gorm:"column:passwd"`
+	Avatar        string `gorm:"column:avatar"`
+	Background    string `gorm:"column:background"`
+	Signature     string `gorm:"column:signature"`
+	FollowCount   int64  `gorm:"column:follow_count"`
+	FollowerCount int64  `gorm:"column:follower_count"`
 }
 
 func (SQLUser) TableName() string {
