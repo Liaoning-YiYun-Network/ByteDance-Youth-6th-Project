4e67805bbdacc9e4b29cd835b4c89472f5949f30
wangletian <wangletian134132@163.com>
2023-08-02 12:54:24 +0800

    修正了sql链接方式

--- go/dao/mysql.go
+++ go/dao/mysql.go
@@ -1,11 +1,10 @@
 package dao
 
 import (
-	"SkyLine/data"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
-	"gopkg.in/yaml.v3"
-	"os"
+	"github.com/spf13/viper"
 )
 
 type MySQLConfig struct {
@@ -17,23 +16,11 @@ type MySQLConfig struct {
 }
 
 func (c *MySQLConfig) getConf() *MySQLConfig {
-	//读取resources/mysql.yaml文件
-	var yamlFile []byte
-	var err error
-	if data.OS == "windows" {
-		yamlFile, err = os.ReadFile("../resources/mysql.yaml")
-	} else {
-		yamlFile, err = os.ReadFile("resources/mysql.yaml")
-	}
-	//若出现错误，打印错误提示
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	//将读取的字符串转换成结构体conf
-	err = yaml.Unmarshal(yamlFile, c)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
+	c.Url = viper.GetString("db.url")
+	c.UserName = viper.GetString("db.userName")
+	c.Password = viper.GetString("db.password")
+	c.DbName = viper.GetString("db.dbname")
+	c.Port = viper.GetString("db.port")
 	return c
 }
 
@@ -46,6 +33,7 @@ func InitMySql() (err error) {
 	var c MySQLConfig
 	//获取yaml配置参数
 	conf := c.getConf()
+	fmt.Printf("数据库配置文件参数：%#v\n", conf)
 	//将yaml配置参数拼接成连接数据库的url
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		conf.UserName,
