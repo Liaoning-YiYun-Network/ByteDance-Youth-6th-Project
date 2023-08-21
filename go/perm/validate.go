47bbfbbdd9cc28f5c07995612b3267e487d51162
ZQDesigned <2990918167@QQ.COM>
2023-08-21 20:00:23 +0800

    改写校验token机制

--- go/perm/validate.go
+++ go/perm/validate.go
@@ -2,23 +2,23 @@ package perm
 
 import (
 	"SkyLine/dao"
+	"SkyLine/entity"
 	"SkyLine/service"
-	"strconv"
 )
 
 // ValidateToken 验证token是否有效
 //
 // token: token
 //
-// return: bool 是否有效, string true: 用户id/false: 错误信息
-func ValidateToken(token string) (bool, string) {
+// return: bool 是否有效, string false: 错误信息, entity.SQLUser true: 用户信息
+func ValidateToken(token string) (bool, string, *entity.SQLUser) {
 	username, err := dao.GetRedis(token)
 	if err != nil {
-		return false, "Token Invalid  or expired"
+		return false, "Token Invalid  or expired", new(entity.SQLUser)
 	}
 	user, err := service.GetSQLUserByName(username)
 	if err != nil {
-		return false, "User doesn't exist"
+		return false, "User doesn't exist", new(entity.SQLUser)
 	}
-	return true, strconv.FormatInt(user.UserId, 10)
+	return true, "", user
 }
