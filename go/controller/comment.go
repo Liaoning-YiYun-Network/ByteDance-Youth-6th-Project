df302ebb20c8078cd33debe3825289c90a3daeb7
ZQDesigned <skychushou708825@outlook.com>
2023-08-26 20:01:49 +0800

    完善comment部分代码，应该能跑了吧（？）

--- go/controller/comment.go
+++ go/controller/comment.go
@@ -22,11 +22,14 @@ type CommentActionResponse struct {
 	Comment entity.Comment `json:"comment,omitempty"`
 }
 
+var tmpIdSequence map[int]int64
+
+var tmpIdInitialized map[int]bool
+
 // CommentAction add or delete comment
 func CommentAction(c *gin.Context) {
 	token := c.Query("token")
-	vid := c.Query("video_id")
-	vidInt, _ := strconv.Atoi(vid)
+	vidInt, _ := strconv.Atoi(c.Query("video_id"))
 	sv, err := service.GetSQLVideoById(vidInt)
 
 	if err != nil {
@@ -35,29 +38,52 @@ func CommentAction(c *gin.Context) {
 		})
 		return
 	}
+	if tmpIdInitialized[vidInt] == false {
+		cs, err := service.GetAllCommentsByDBName(sv.CommentDB)
+		if err != nil {
+			data.Logger.Error("Failed to init comments: ", err)
+			c.JSON(http.StatusOK, CommentActionResponse{
+				Response: entity.Response{StatusCode: 1, StatusMsg: "Failed to get comments"},
+			})
+			return
+		}
+		//获取列表中最大的id
+		maxId := int64(0)
+		for _, c := range cs {
+			if c.CommentID > maxId {
+				maxId = c.CommentID
+			}
+		}
+		tmpIdSequence[vidInt] = maxId + 1
+	}
 	isValid, msg, user := perm.ValidateToken(token)
 	actionType := c.Query("action_type")
 	// 检查用户登录状态
 	if isValid {
 		if actionType == "1" {
-			all, err := service.GetAllCommentsByDBName(sv.CommentDB)
-			commentID := len(all) + 1
 			// 获取评论内容
 			commentText := c.Query("comment_text")
-
+			// 获取评论用户
+			user, err := type_conv.ToUser(*user)
+			if err != nil {
+				data.Logger.Error("Failed to convert user: ", err)
+				c.JSON(http.StatusOK, CommentActionResponse{
+					Response: entity.Response{StatusCode: 1, StatusMsg: "Failed to convert user"},
+				})
+				return
+			}
 			// 创建评论对象
 			newComment := entity.Comment{
-				Id:         int64(commentID),
-				User:       type_conv.ToUser(*user),
+				Id:         tmpIdSequence[vidInt],
+				User:       user,
 				Content:    commentText,
-				CreateDate: time.Now().Format("05-15"),
+				CreateDate: strconv.FormatInt(time.Now().Unix(), 10),
 			}
 			// 在数据库中添加评论
 			err = service.AddCommentByDBName(sv.CommentDB, entity.DBComment{
-				CommentID: newComment.Id,
-				UserID:    newComment.User.Id,
-				Content:   newComment.Content,
-				Time:      newComment.CreateDate,
+				UserID:  newComment.User.Id,
+				Content: newComment.Content,
+				Time:    newComment.CreateDate,
 			})
 			if err != nil {
 				c.JSON(http.StatusOK, CommentActionResponse{
@@ -72,13 +98,13 @@ func CommentAction(c *gin.Context) {
 			})
 			return
 		} else {
-			commentIDStr := c.Query("comment_id")
-			commentID, _ := strconv.ParseInt(commentIDStr, 10, 64)
+			// ！！！由于此处的数值是从客户端传过来的，所以不考虑出现异常的情况。但是在Restful API中，应该考虑到异常情况。
+			commentID, _ := strconv.ParseInt(c.Query("comment_id"), 10, 64)
 
 			// 调用删除评论的函数
 			err := service.DeleteCommentByDBName(sv.CommentDB, commentID)
 			if err != nil {
-				c.JSON(http.StatusInternalServerError, CommentActionResponse{
+				c.JSON(http.StatusOK, CommentActionResponse{
 					Response: entity.Response{StatusCode: 1, StatusMsg: "Failed to delete comment"},
 				})
 				return
@@ -100,6 +126,7 @@ func CommentAction(c *gin.Context) {
 
 // CommentList 获取视频的所有评论，并按发布时间倒序排列
 func CommentList(c *gin.Context) {
+	// ！！！由于此处的数值是从客户端传过来的，所以不考虑出现异常的情况。但是在Restful API中，应该考虑到异常情况。
 	videoID, _ := strconv.Atoi(c.Query("video_id")) // 从请求中获取视频标识符
 	sv, err := service.GetSQLVideoById(videoID)
 	// 使用 videoID 获取与该视频相关的评论数据，按发布时间倒序排列
@@ -126,19 +153,16 @@ func CommentList(c *gin.Context) {
 					Name:          ud.Name,
 					FollowCount:   ud.FollowCount,
 					FollowerCount: ud.FollowerCount,
-				}, // 假设 User 有一个 Id 字段
+				},
 				Content:    dbComment.Content,
 				CreateDate: dbComment.Time,
 			})
 		}
 	}
 
-	// 构造响应结构体
-	response := CommentListResponse{
+	// 返回响应
+	c.JSON(http.StatusOK, CommentListResponse{
 		Response:    entity.Response{StatusCode: 0},
 		CommentList: comments,
-	}
-
-	// 返回响应
-	c.JSON(http.StatusOK, response)
+	})
 }
